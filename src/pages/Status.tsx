27ebc819ad525a3461821daf621b3613c62f2657
Beatriz Fernandes <101184559+btwbeatriz@users.noreply.github.com>
2023-02-13 22:12:08 -0300

    initial commit

---
+++ src/pages/Status.tsx
@@ -0,0 +1,66 @@
+import { PaperPlaneRight } from "phosphor-react";
+import { FormEvent, KeyboardEvent, useState } from "react";
+import { Header } from "../components/Header"
+import { Separator } from "../components/Separator"
+import { Tweet } from "../components/Tweet"
+
+import './Status.css';
+
+export function Status(){
+
+    const [newAnswer, setNewAnswer] = useState('')
+    const [answers, setAnswers] = useState([
+        'Concordo...',
+        'Olha, faz sentido',
+        'Parabéns pelo progresso!'
+    ])
+
+    function createNewAnswer(event: FormEvent){
+    event.preventDefault()
+    
+    setAnswers([newAnswer, ...answers])
+    setNewAnswer('')
+  }
+
+  function handleHotKeySubmit(event: KeyboardEvent){
+    if (event.key === 'Enter' && (event.ctrlKey || event.metaKey)){
+        setAnswers([newAnswer, ...answers])
+        setNewAnswer('')
+    }
+  }
+
+    return(
+        <main className='status'>
+          <Header title="Tweet" />
+
+        <Tweet content="Estudando React e adorando!" />
+
+        <Separator />
+
+        <form onSubmit={createNewAnswer} className='answer-tweet-form'>
+            <label htmlFor='tweet'>
+              <img src="https://github.com/btwbeatriz.png" alt="Beatriz Fernandes" />
+              <textarea 
+              id='tweet' 
+              placeholder="Tweet your answer"
+              value={newAnswer}
+              onKeyDown={handleHotKeySubmit}
+              onChange={(event) => {
+                setNewAnswer(event.target.value)
+              }}
+              />
+            </label>
+
+            <button type='submit'>
+                <PaperPlaneRight />
+                <span>Answer</span>
+            </button>
+        </form>
+          
+         {answers.map(answer => {
+            return <Tweet key={answer} content={answer} />
+        })}
+
+        </main>
+    )
+}
